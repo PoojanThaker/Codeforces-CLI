066750a99899218cbd1e185dc2cabaf35a19232b
Varshil Gandhi <vagandhi@microsoft.com>
2020-10-30 01:11:31 +0530

    Add class Database Connection and put_problem method

--- Scrapper/constants.py
+++ Scrapper/constants.py
@@ -2,3 +2,10 @@ CODEFORCES = 'codeforces'
 CODEFORCES_URL = 'https://codeforces.com/'
 CODEFORCES_PROBLEMSET_URL = 'https://codeforces.com/problemset'
 CODEFORCES_SCRAPED_PROBLEMS_FILE = 'scraped_list_cf.csv'
+
+# AWS database parameters
+ENDPOINT = 'database-1.cs1dyqhidm16.ap-south-1.rds.amazonaws.com'
+PORT = 5432
+DBNAME = 'myDatabase'
+USER = 'lockouts'
+PASSWORD = 'password'
--- Scrapper/database_connect.py
+++ Scrapper/database_connect.py
@@ -1,1 +1,40 @@
-#TODO by varshil
+import psycopg2
+from Scrapper.constants import *
+import pandas as pd
+
+
+class DatabaseConnect:
+    def __init__(self):
+        try:
+            self.conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD)
+            self.cur = self.conn.cursor()
+            # For the first time, implement table
+            # self.cur.execute("""CREATE TABLE problems(
+            #                                 id text PRIMARY KEY NOT NULL,
+            #                                 url text NOT NULL,
+            #                                 platform text,
+            #                                 metadata json,
+            #                                 statement json)"""
+            #                     )
+            # self.conn.commit()
+            self.check_problems_table_exist()
+        except Exception as e:
+            print("Database connection failed due to the following issue: {}".format(e))
+
+    def check_problems_table_exist(self):
+        sql = """
+                SELECT "table_name","column_name", "data_type", "table_schema"
+                FROM INFORMATION_SCHEMA.COLUMNS
+                WHERE "table_schema" = 'public'
+                ORDER BY table_name
+                """
+        info = pd.read_sql(sql, con=self.conn)
+        df = info[info.table_name == 'problems']
+        df = df.column_name.isin(['id', 'url', 'platform', 'metadata', 'statement']).all()
+        assert df, "The table \'problems\' does not exist in the database."
+
+    def put_problems(self, problem):
+        sql = """ INSERT INTO problems(id, url, platform, metadata, statement)
+                    VALUES (%s, %s, %s, %s, %s) RETURNING *;"""
+        self.cur.execute(sql, (problem.id, problem.url, problem.platform, problem.metadata, problem.statement))
+        self.conn.commit()
